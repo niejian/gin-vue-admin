c566c7e04f51a713b4f370aeeb2a1ef4aeddd60b
聂剑 <niejian9001@163.com>
2021-06-21 14:23:38 +0800

    配置信息设置到pod环境变量中

--- server/core/config.go
+++ server/core/config.go
@@ -6,6 +6,8 @@ import (
 	_ "gin-vue-admin/packfile"
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
+	"os"
+	"strings"
 )
 
 const defaultConfigFile = "config.yaml"
@@ -30,4 +32,29 @@ func init() {
 		fmt.Println(err)
 	}
 	global.GVA_VP = v
+	// 判断是否是k8s环境，如果是，那么数据库，es读取k8s部署文件中的配置信息
+	enableK8s := os.Getenv("ENABLE_K8S")
+	if "" != enableK8s && "true" == enableK8s {
+		fmt.Println("读取k8s配置信息 ")
+		esUrls := os.Getenv("ES_URLS")
+		esUsername := os.Getenv("ES_USERNAME")
+		esPassword := os.Getenv("ES_PASSWORD")
+
+		if "" != esUrls && "" != esUsername && "" != esPassword {
+			// 替换
+			global.GVA_CONFIG.Es.Urls = strings.Split(esUrls, ",")
+			global.GVA_CONFIG.Es.Username = esUsername
+			global.GVA_CONFIG.Es.Password = esPassword
+		}
+
+		mysqlUrl := os.Getenv("MYSQL_URLS")
+		mysqlUsername := os.Getenv("MYSQL_USERNAME")
+		mysqlPassword := os.Getenv("MYSQL_PASSWORD")
+
+		if "" != mysqlUrl && "" != mysqlUsername && "" != mysqlPassword {
+			global.GVA_CONFIG.Mysql.Path = mysqlUrl
+			global.GVA_CONFIG.Mysql.Username = mysqlUsername
+			global.GVA_CONFIG.Mysql.Password = mysqlPassword
+		}
+	}
 }
